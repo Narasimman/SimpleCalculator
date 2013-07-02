54ac679e706f29a5027ad4d77716d8fa41c71659
Narasimman <narasi.mit@gmail.com>
2013-07-02 21:38:32 +0530

    update android studio

--- TouchCalc/build/source/r/debug/com/example/touchcalc/R.java
+++ TouchCalc/build/source/r/debug/com/example/touchcalc/R.java
@@ -10,30 +10,50 @@ package com.example.touchcalc;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int darkgreen=0x7f040000;
+        public static final int darkgrey=0x7f040001;
+        public static final int lightyellow=0x7f040002;
+    }
     public static final class dimen {
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int appvertical=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int inputview=0x7f020002;
+        public static final int keypad=0x7f020003;
+        public static final int keypadclear=0x7f020004;
+        public static final int keypadmembuffer=0x7f020005;
+        public static final int keypadop=0x7f020006;
+        public static final int keypadother=0x7f020007;
+        public static final int keypadresult=0x7f020008;
+        public static final int stackview=0x7f020009;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int grdButtons=0x7f080003;
-        public static final int txtInput=0x7f080001;
-        public static final int txtMemory=0x7f080002;
-        public static final int txtStack=0x7f080000;
+        public static final int action_settings=0x7f090009;
+        public static final int editText=0x7f090005;
+        public static final int editText2=0x7f090006;
+        public static final int grdButtons=0x7f090003;
+        public static final int spinner=0x7f090004;
+        public static final int spinner2=0x7f090007;
+        public static final int spinner3=0x7f090008;
+        public static final int txtInput=0x7f090001;
+        public static final int txtMemory=0x7f090002;
+        public static final int txtStack=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int convertor=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050000;
-        public static final int app_name=0x7f050001;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060000;
+        public static final int app_name=0x7f060001;
+        public static final int hello_world=0x7f060002;
     }
     public static final class style {
         /** 
@@ -44,9 +64,9 @@ public final class R {
  API 11 theme customizations can go here. 
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
